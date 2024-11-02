cdf4bb8bd50afd39e79c857541a1b6336d9180f1
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-11-02 21:36:07 +0200

    Update dz5.js

--- dz5.js
+++ dz5.js
@@ -1,113 +1,113 @@
-let select = prompt("Виберіть між кава, сік та вода")
-let drink = select
-switch (drink) {
-    case "кава" :
-    alert("Ви вибрали каву");
-    break;
-    case "сік" :
-    alert("Ви вибрали сік");
-    break;
-    case "вода" :
-    alert("Ви вибрали воду");
-    break;
+// let select = prompt("Виберіть між кава, сік та вода")
+// let drink = select
+// switch (drink) {
+//     case "кава" :
+//     alert("Ви вибрали каву");
+//     break;
+//     case "сік" :
+//     alert("Ви вибрали сік");
+//     break;
+//     case "вода" :
+//     alert("Ви вибрали воду");
+//     break;
     
-}
+// }
 
-let day = prompt("Який сьогодні день тижня?")
+// let day = prompt("Який сьогодні день тижня?")
 
-switch (day) {
-    case "понеділок" :
-    case "вівторок" :
-    case "середа" :
-    case "четвер" :
-    case "пятниця" :
-    alert("сьогодні не вихідний");
-    break;
-    case "субота" :
-    case "неділя" :
-    alert("сьогодні вихідний")   
-}
+// switch (day) {
+//     case "понеділок" :
+//     case "вівторок" :
+//     case "середа" :
+//     case "четвер" :
+//     case "пятниця" :
+//     alert("сьогодні не вихідний");
+//     break;
+//     case "субота" :
+//     case "неділя" :
+//     alert("сьогодні вихідний")   
+// }
 
-let month = prompt("Введіть номер місяця")
-switch (month) {
-    case "12" :
-    case "1" :
-    case "2" :
-    alert("Це зима")
-    break;
-    case "3" :
-    case "4" :
-    case "5" :
-    alert("Це весна")
-    break;
-    case "6" :
-    case "7" :
-    case "8" :
-    alert("КАНІКУЛИ")
-    break;
-    case "9" :
-    case "10" :
-    case "11" :
-    alert("Це осінь")
-    break;
-    default:
-    alert("Ви шось не то ввели")
-}
+// let month = prompt("Введіть номер місяця")
+// switch (month) {
+//     case "12" :
+//     case "1" :
+//     case "2" :
+//     alert("Це зима")
+//     break;
+//     case "3" :
+//     case "4" :
+//     case "5" :
+//     alert("Це весна")
+//     break;
+//     case "6" :
+//     case "7" :
+//     case "8" :
+//     alert("КАНІКУЛИ")
+//     break;
+//     case "9" :
+//     case "10" :
+//     case "11" :
+//     alert("Це осінь")
+//     break;
+//     default:
+//     alert("Ви шось не то ввели")
+// }
 
-let monthDay = prompt("Введіть номер місяця")
-switch (monthDay) {
-    case "8" :
-    case "1" :
-    case "4" :
-    alert("30")
-    break;
-    case "6" :
-    case "7" :
-    case "11" :
-    case "3" :
-    case "9" :
-    alert("31")
-    break;
-    case "2" :
-    case "10" :
-    case "12" :
-    alert("29")
-    break;
-    default:
-    alert("Ви шось не то ввели")
-}
+// let monthDay = prompt("Введіть номер місяця")
+// switch (monthDay) {
+//     case "8" :
+//     case "1" :
+//     case "4" :
+//     alert("30")
+//     break;
+//     case "6" :
+//     case "7" :
+//     case "11" :
+//     case "3" :
+//     case "9" :
+//     alert("31")
+//     break;
+//     case "2" :
+//     case "10" :
+//     case "12" :
+//     alert("29")
+//     break;
+//     default:
+//     alert("Ви шось не то ввели")
+// }
 
-let color = prompt("Введіть колір світлофора")
-switch (monthDay) {
-    case "Зелений" :
-    alert("Ідіть")
-    break;
-    case "Червоний" :
-    alert("СТОП")
-    break;
-    case "Жовтий" :
-    alert("Зачекайте будь ласка")
-    break;
-    default:
-    alert("Попробуйте ще раз ввести кольори світлофора")
-}
+// let color = prompt("Введіть колір світлофора")
+// switch (monthDay) {
+//     case "Зелений" :
+//     alert("Ідіть")
+//     break;
+//     case "Червоний" :
+//     alert("СТОП")
+//     break;
+//     case "Жовтий" :
+//     alert("Зачекайте будь ласка")
+//     break;
+//     default:
+//     alert("Попробуйте ще раз ввести кольори світлофора")
+// }
 
-let x = prompt(Number("Введіть цифру(x)"))
-let y = prompt(Number("Введіть цифру(y)"))
-let math = prompt("Виберіть або * або + або - або /")
-switch (math) {
-    case "*" :
-    alert(x * y);
-    break;
-    case "+" :
-    alert(x + y);
-    break;
-    case "/" :
-    alert(x / y);
-    break;
-    case "-" :
-    alert(x - y);
-    break;
-    default:
-        alert("Щось неправильно")
-}
+// let x = prompt(Number("Введіть цифру(x)"))
+// let y = prompt(Number("Введіть цифру(y)"))
+// let math = prompt("Виберіть або * або + або - або /")
+// switch (math) {
+//     case "*" :
+//     alert(x * y);
+//     break;
+//     case "+" :
+//     alert(x + y);
+//     break;
+//     case "/" :
+//     alert(x / y);
+//     break;
+//     case "-" :
+//     alert(x - y);
+//     break;
+//     default:
+//         alert("Щось неправильно")
+// }
