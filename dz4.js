00a4a902d482587fe920ef6650557552d2decf90
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-11-02 17:53:58 +0200

    yes

---
+++ dz4.js
@@ -0,0 +1,51 @@
+// const numberOne = prompt("введіть значення")
+// const wordOne = numberOne > 10;
+// console.log(numberOne)
+// if (wordOne) {
+//     alert("більше ніж 10")
+// } else {
+//     alert("менше або дорівнює 10")
+// }
+
+
+// const ifJava = prompt("Do u know JavaScript")
+// const word = ifJava === 'JavaScript';
+// console.log(ifJava)
+// if (word) {
+//     alert("haha i know that u got word JavaScript in the sentence")
+// } else {
+//     alert("no word JavaScript in the sentence")
+// }
+
+
+// const number = prompt("10 <= x <= 20")
+// const ifNumber = number >= 10 && number <= 20;
+// console.log(number)
+// if (ifNumber) {
+//     alert("Число входить в діапазон від 10 до 20")
+// } else {
+//     alert("Число не входить в діапазон від 10 до 20")
+// }
+
+
+// const name = prompt('name');
+// const gmail = prompt('gmail');
+// const password = prompt('password');
+// console.log(name);
+// if (name.length > 3) {
+//     alert('Nice')
+// } else {
+//     alert('Not right!')
+// }
+// console.log(gmail);
+// if (gmail === '@gmail.com' || gmail === '@ukr.net' || gmail === '@hotmail.com') {
+//     alert('Nice')
+// } else {
+//     alert('Not right!')
+// }
+// console.log(password);
+// if (password.length > 6) {
+//     alert('Password is too short')
+// } else {
+//     alert('Not right!')
+// }
