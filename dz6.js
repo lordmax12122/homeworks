bcc8a5362f3c0b94a137a46927ee79252bdba879
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-11-03 10:37:21 +0200

    ok

---
+++ dz6.js
@@ -0,0 +1,54 @@
+// let i = 1
+// while (i <= 10) {
+//     i + 1
+// }
+// console.log(i)
+
+
+
+// for (let i = 0; i <= 20; i + 2) {
+// console.log(i)
+// }
+
+
+// const x = 7;
+// for (let y = 1; y <= 10; y += 1) {
+//     let z = x * y
+//     console.log(z)
+// }
+
+
+// const r = [1, 2, 3, 4, 5];
+// let a = 1;
+// while (o <= r.length) {
+//     o + 1
+//     console.log(0)
+// }
+
+
+// const r = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let a = 1; a <= r.length; a + 1) {
+//     if (a = 7) {
+//     break;
+//     }
+// console.log(a)    
+// }
+
+
+// const z = 100;
+// for (let n = 1; n <= z; n + 10) {
+//     if (n >= 100) {
+//     brenk;
+//     }
+// console.log(n)    
+// }
+
+// const a = 1;
+// while (a < 20) {
+//     if (a % 3) {
+//         a + 1
+//         continue;
+//     }
+//     console.log(a);
+// }
+
