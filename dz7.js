048160ec3f592e6729e651b485e3c6bc7ce51785
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-11-16 20:18:25 +0200

    yes

---
+++ dz7.js
@@ -0,0 +1,39 @@
+// let x = [1, 2, 3]
+// x[1] = 10
+// console.log[x];
+
+// let y = [1, 2, 3]
+// y[3] = 4
+// console.log(y);
+
+// let numbers = [1, 2, 3]
+// let sum = numbers[1] + numbers[2] + numbers[0]
+// console.log(sum);
+
+// let numbers2 = [1, 2, 3, 4, 5]
+// for (let i = 0; i < numbers2.length; i + 1) {
+//     console.log(numbers2[i]);
+// }
+
+// let names = ["Sasha", "Markiyan", "Maksym", "Dominik", "Yura"] 
+// if (names.length > 5) {
+//     console.log(names)
+// }
+
+// let numbers3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let maxNumber = numbers3[0]; 
+// for (let i = 1; i < numbers3.length; i + 1) {
+//   if (numbers3[i] > maxNumber) {
+//     maxNumber = numbers3[i];
+//   }
+// }
+// console.log(maxNumber);
+
+// let numbers4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let maxNumber = numbers4[0]; 
+// for (let i = 1; i < numbers4.length; i + 1) {
+//   if (numbers4[i] % 2 === 0) {
+//   console.log(maxNumber[i]);
+//   }
+// }
+
