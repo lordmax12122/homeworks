b4f7a59329e3c714938d21cb01b84b8f3743b38d
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-11-30 18:58:27 +0200

    a

---
+++ dz8.js
@@ -0,0 +1,15 @@
+// const friends = ['Mango', 'Poly', 'Kiwi', 'Ajax'];
+// const friend = friends.join(" ");
+// console.log(friend)
+
+const cards = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+];
+const card = cards.splice(2, 1)
+const pushCard = cards.splice(5, 1, '6')
+const updateCard4 = cards.splice(2, 1, '4.1');
+console.log(cards)
