96d5e7bc9a65bb5a8193ca15a67b89c8115d39ec
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2024-12-07 22:00:43 +0200

    complete

---
+++ dz9.js
@@ -0,0 +1,52 @@
+// const array = ['Egg ', 'Potato ', 'Trex '];
+// let result = "";
+// function logItems(array) {
+// for(let i = 0; i < array.length; i++) {
+//     result += `${i} - ${array[i]}`;
+// }
+// return result
+// } 
+// console.log(logItems(array))
+
+// const sentence = 'Calculate Engraving Price';
+// function calculateEngravingPrice(message, pricePerWord) {
+//     const words = message.split(" ");
+//     const sumWords = words.length;
+//     return sumWords * pricePerWord;
+// }
+// console.log(calculateEngravingPrice(sentence, 52));
+
+// const sentence = 'Longest word is';
+// const words = sentence.split(" ");
+// let longestWord = "";
+// function findLongestWord(string) {
+//     for(let i = 0; i < words.length; i++) {
+//         if (words[i].length > longestWord) {
+//             longestWord = words[i];
+//     }
+//     }
+//     return longestWord;
+// }
+
+// console.log(findLongestWord(words));
+
+// const sentence = 'This sentence has less than 40 symbols, or not'
+// function formatString(string) {
+//     if(sentence.length > 40) {
+//         return sentence.slice(0, 40) + '...';
+//     }
+//     else {
+//         return sentence;
+//     }
+// }
+// console.log(formatString(sentence))
+
+// const sentence = 'not spam'
+// function checkForSpam(message) {
+//     if (sentence.includes('spam','sale')) {
+//         return false
+//     } else {
+//         return true
+//     }     
+// }
+// console.log(checkForSpam(sentence))
